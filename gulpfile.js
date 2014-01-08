86ef8d289ac1dfe6ca83d90fc9d37e5904add36b
Artem Medeusheyev <artem.medeu@gmail.com>
2014-01-08 17:24:27 +0600

    default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 
-gulp.task('css', function() {
+gulp.task('default', function() {
   gulp.src(['client/css/**/*.less', '!client/css/includes/*.less'])
     .pipe(less().on('error', console.log))
     .pipe(gulp.dest('dist/css'));
